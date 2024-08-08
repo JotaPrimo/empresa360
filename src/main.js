e5194c11d8c756e56aa8f8382a656b8027f7189c
jota santos <gestald118@email.com>
2024-08-08 07:58:20 -0400

    216. Configurando o Vue Router em um projeto Vue 3

--- src/main.js
+++ src/main.js
@@ -1,4 +1,36 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+
+// é no mais .js que configura
+// inicio das rotas
+import { createRouter, createWebHashHistory } from 'vue-router'
+
+// componentes
+import HomePage from './views/HomePage.vue'
+import LoginPage from './views/LoginPage.vue'
+
+
+
+const routes = [
+    {
+        path: '/home',
+        component: HomePage
+    },
+    {
+        path: '/login',
+        component: LoginPage
+    }
+]
+
+// aplicando rotas ao contexto do vue
+const router = createRouter({
+    history: createWebHashHistory(),
+    routes: routes
+});
+
+//fim das rotas
+const Vue = createApp(App)
+Vue.use(router)
+Vue.mount('#app')
+
---
+++ src/views/HomePage.vue
@@ -0,0 +1,11 @@
+<template>
+    <h3>
+        Home
+    </h3>
+</template>
+
+<script>
+export default {
+    name: "HomePage"
+}
+</script>
